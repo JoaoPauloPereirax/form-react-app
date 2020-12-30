42999459846fcd953ea3e92dbdf316f3654ec9f7
João Paulo Pereira <joaopaulomatematicapho@gmail.com>
2020-12-29 21:10:16 -0300

    Creating form static

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import './global.css'
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="login-container" >
+      <h1>Página de login</h1> <hr/>
+      <form >
+        <label>
+          <h2>Email: </h2>
+          <input type="email" name="email"/><br/>
+        </label>
+        <label>
+          <h2>Senha: </h2>
+          <input type="password" name="password"/><br/>
+        </label>
+        <input type="submit" value="Login"/>
+      </form>
+    </div>
   );
 }
 
