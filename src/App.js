ca19aa11e1d7667c140c993c238ed21657516622
João Paulo Pereira <joaopaulomatematicapho@gmail.com>
2020-12-30 01:11:38 -0300

    Adding requests

--- src/App.js
+++ src/App.js
@@ -1,21 +1,103 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './global.css'
 
 function App() {
+
+  const [emailField,setEmailField] = useState('') 
+  const [passwordField,setPasswordField] = useState('')
+  const [nameField,setNameField] = useState('')
+  const [loading,setLoading] = useState(true)
+  
+
+  const handleLoginSubmit = async (event)=>{
+   
+    let url = "https://api.b7web.com.br/carros/api/auth/login"
+    let result = await fetch(url,{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify({
+        email: emailField,
+        password: passwordField
+      })
+    })
+    let json = await result.json()
+
+    if(json !== ''){
+      alert(json.error)
+    }
+      console.log("Result: ",json)
+    
+    }
+
+    const handleRegisterSubmit = async ()=>{
+      let url = "https://api.b7web.com.br/carros/api/auth/register"
+      let result = await fetch(url,{
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body: JSON.stringify({
+          name: nameField,
+          email: emailField,
+          password: passwordField
+        })
+      })
+      let json = await result.json()
+  
+      if(json !== ''){
+        alert(json.error)
+      }
+        console.log("Result: ",json)
+    }
+
+
   return (
     <div className="login-container" >
-      <h1>Página de login</h1> <hr/>
-      <form >
+      Login
+      <input type="radio" name="opcao" onChange={()=>setLoading(true)} />
+      Cadastro
+      <input type="radio" name="opcao" onChange={()=>setLoading(false)} />
+      
+      {loading && 
+          <div>
+            <h1>Página de login</h1> <hr/>
+            <form onSubmit={handleLoginSubmit} >
+              <label>
+                <h2>Email: </h2>
+                <input type="email" value={emailField} onChange={event=>{setEmailField(event.target.value)}} /><br/>
+              </label>
+              <label>
+                <h2>Senha: </h2>
+                <input type="password" value={passwordField} onChange={event=>{setPasswordField(event.target.value)}} /><br/>
+              </label>
+              <input type="submit" value="Login"/>
+            </form>
+          </div>
+      }
+      {
+        !loading &&
+        <div>
+      <h1>Página de Cadastro</h1> <hr/>
+      <form onSubmit={handleRegisterSubmit} >
+        <label>
+          <h2>Nome: </h2>
+          <input type="text" value={nameField} onChange={event=>setNameField(event.target.value)} />
+        </label>
         <label>
           <h2>Email: </h2>
-          <input type="email" name="email"/><br/>
+          <input type="email" value={emailField} onChange={event=>{setEmailField(event.target.value)}} /><br/>
         </label>
         <label>
           <h2>Senha: </h2>
-          <input type="password" name="password"/><br/>
+          <input type="password" value={passwordField} onChange={event=>{setPasswordField(event.target.value)}} /><br/>
         </label>
         <input type="submit" value="Login"/>
       </form>
+        </div>
+      }
+
     </div>
   );
 }
